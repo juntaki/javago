f8c5f1db78283067f2df236d8d6b50f9b8442d10
juntaki <me@juntaki.com>
2017-10-12 01:06:24 +0900

    Fix template

--- javap.go
+++ javap.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os/exec"
 	"regexp"
 	"strings"
 )
 
+var funcSignagure = regexp.MustCompile(`\((.*)\).*`)
+
 func runJavap(classpath, importpath string) Clazz {
 	cmd := exec.Command("javap", "-public", "-s", "-classpath", classpath, importpath)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		panic("wa")
-		//return nil, err
+		panic("Failed to exec javap")
 	}
 
 	re := regexp.MustCompile(`\s(\S*?)(|\(.*?\))\s*(|throws.*?);`)
@@ -36,8 +38,12 @@ func runJavap(classpath, importpath string) Clazz {
 
 	importpathArray := strings.Split(importpath, ".")
 	clazz := Clazz{
-		Name:    importpathArray[len(importpathArray)-1],
-		Members: make([]Member, 0),
+		Name:           importpathArray[len(importpathArray)-1],
+		FQCN:           strings.Replace(importpath, ".", "/", -1),
+		StaticFunction: make(map[string]StaticFunction, 0),
+		Function:       make(map[string]Function, 0),
+		StaticVariable: make([]StaticVariable, 0),
+		Variable:       make([]Variable, 0),
 	}
 scan:
 	for scanner.Scan() {
@@ -75,25 +81,79 @@ scan:
 
 		signature := next[len(next)-1]
 
-		clazz.Members = append(clazz.Members, Member{
-			Symbol:    symbol,
-			Signature: signature,
-			Static:    static,
-			Function:  function,
-		})
+		if function {
+			argSig := funcSignagure.FindStringSubmatch(signature)[1]
+			if static {
+				if staticFunction, ok := clazz.StaticFunction[symbol]; ok {
+					staticFunction.Signature[argSig] = signature
+				} else {
+					clazz.StaticFunction[symbol] = StaticFunction{
+						Symbol:    symbol,
+						Signature: map[string]string{argSig: signature},
+					}
+				}
+			} else {
+				if function, ok := clazz.Function[symbol]; ok {
+					function.Signature[argSig] = signature
+				} else {
+					clazz.Function[symbol] = Function{
+						Symbol:    symbol,
+						Signature: map[string]string{argSig: signature},
+					}
+				}
+			}
+		} else {
+			if static {
+				clazz.StaticVariable = append(clazz.StaticVariable, StaticVariable{
+					Symbol:    symbol,
+					Signature: signature,
+				})
+			} else {
+				clazz.Variable = append(clazz.Variable, Variable{
+					Symbol:    symbol,
+					Signature: signature,
+				})
+			}
+		}
+
 	}
 	cmd.Wait()
 	return clazz
 }
 
 type Clazz struct {
-	Name    string
-	Members []Member
+	Name           string
+	FQCN           string
+	StaticFunction map[string]StaticFunction
+	Function       map[string]Function
+	StaticVariable []StaticVariable
+	Variable       []Variable
 }
 
-type Member struct {
+type StaticFunction struct {
+	Symbol    string
+	Signature map[string]string // [Arg]Full
+}
+
+func (f StaticFunction) SignatureMap() string {
+	return fmt.Sprintf("%#v", f.Signature)
+}
+
+type Function struct {
+	Symbol    string
+	Signature map[string]string // [Arg]Full
+}
+
+func (f Function) SignatureMap() string {
+	return fmt.Sprintf("%#v", f.Signature)
+}
+
+type StaticVariable struct {
 	Symbol    string
 	Signature string
-	Static    bool
-	Function  bool
+}
+
+type Variable struct {
+	Symbol    string
+	Signature string
 }
