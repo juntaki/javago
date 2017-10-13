59f5329aef3aba45fa723f2aed4d8f59fab9946a
Jumpei Takiyasu <jumpei-takiyasu@m3.com>
2017-10-13 11:39:51 +0900

    Fix

--- main.go
+++ main.go
@@ -7,6 +7,77 @@ import (
 	"github.com/urfave/cli"
 )
 
+const templ string = `
+package java
+
+import "github.com/juntaki/jnigo"
+
+var jvm *jnigo.JVM
+
+func init() {
+	jvm = jnigo.CreateJVM()
+}
+
+type {{.Name}} struct{
+	jclass *jnigo.JClass
+}
+
+func New{{.Name}}(args ...interface{}) (*{{.Name}}, error) {
+	convertedArgs, err := jvm.ConvertAll(args)
+	if err != nil{
+		return nil, err
+	}
+	jclass, err := jvm.NewJClass("{{.FQCN}}", convertedArgs)
+	if err != nil{
+		return nil, err
+	}
+	return &{{.Name}}{
+		jclass: jclass,
+	}, nil
+}
+{{range .Variable}}
+func (c *{{$.Name}}) Get{{.Symbol}}() (jnigo.JObject, error) {
+	return c.jclass.GetField("{{.Symbol}}", "{{.Signature}}")
+}
+{{end}}
+
+{{range .Variable}}
+func (c *{{$.Name}}) Set{{.Symbol}}(arg interface{}) (error) {
+	convertedArg = jvm.Convert(arg)
+	return c.jclass.SetField("{{.Symbol}}", convertedArg)
+}
+{{end}}
+
+{{range .Function}}
+func (c *{{$.Name}}) {{.Symbol}}(args ...interface{}) (jnigo.JObject, error) {
+	convertedArgs, err := jvm.ConvertAll(args)
+	if err != nil {
+		return nil, err
+	}
+	sigArgs := ""
+	for _, arg := range convertedArgs {
+		sigArgs += arg.Signature()
+	}
+	sigMap := {{.SignatureMap}}
+	return c.jclass.CallFunction("{{.Symbol}}", sigMap[sigArgs], convertedArgs)
+}
+{{end}}
+{{range .StaticFunction}}
+func {{$.Name}}{{.Symbol}}(args ...interface{}) (jnigo.JObject, error) {
+	convertedArgs, err := jvm.ConvertAll(args)
+	if err != nil {
+		return nil, err
+	}
+	sigArgs := ""
+	for _, arg := range convertedArgs {
+		sigArgs += arg.Signature()
+	}
+	sigMap := {{.SignatureMap}}
+	return jvm.CallStaticFunction("{{$.FQCN}}", "{{.Symbol}}", sigMap[sigArgs], convertedArgs)
+}
+{{end}}
+`
+
 func main() {
 	var classpath string
 	var classfile string
@@ -30,8 +101,13 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
+		if classfile == "" {
+			cli.ShowCommandHelpAndExit(c, "javago", 1)
+		}
+
 		clazz := runJavap(classpath, classfile)
-		tmpl := template.Must(template.ParseFiles("class.tmpl"))
+
+		tmpl := template.Must(template.New("class").Parse(templ))
 
 		path := "java"
 		if _, err := os.Stat(path); os.IsNotExist(err) {
